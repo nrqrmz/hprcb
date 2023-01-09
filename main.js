0e4d95f02cc40aca34f9abf033b2b01707df4d57
Enrique Ramirez <nrqrmz@gmail.com>
2023-01-09 16:05:30 -0600

    upload logo

---
+++ main.js
@@ -0,0 +1,6 @@
+const video = document.querySelector('#background-video')
+const content = document.querySelector('#background-div')
+
+video.addEventListener('click', ()=> {
+  video.classList.add('display-none')
+})
