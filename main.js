8408e6cd5a66b9369833df0ce12542c1c17cb557
Enrique Ramirez <nrqrmz@gmail.com>
2023-01-09 16:25:47 -0600

    style logo

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const video = document.querySelector('#background-video')
+const cubeDiv = document.querySelector('#cube-div')
 const content = document.querySelector('#background-div')
+
 
 video.addEventListener('click', ()=> {
   video.classList.add('display-none')
+  cubeDiv.classList.remove('display-none')
+  cubeDiv.classList.add('display-flex-center')
 })
