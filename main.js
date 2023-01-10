1a9d51c74b13d9fd390b11da54b4801b235d5caf
Enrique Ramirez <nrqrmz@gmail.com>
2023-01-10 00:37:15 -0600

    add half of the content

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const video = document.querySelector('#background-video')
+const backVideo = document.querySelector('#background-video')
+const video = document.querySelector('#video-div')
 const content = document.querySelector('#background-div')
 
 video.addEventListener('click', ()=> {
   video.classList.add('display-none')
+  backVideo.classList.add('display-none')
   content.classList.remove('display-none')
 })
-
